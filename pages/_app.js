a1e5f5486b912e1ee153fbd8b6826b3ab56879af
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-04-16 23:50:17 +0100

    initial commit#

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import Layout from '../components/Layout'
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import reducer from '../lib/reducer.js'
+import thunk from 'redux-thunk'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export const store = createStore(reducer, applyMiddleware(thunk))
+
+export default function MyApp({
+  Component,
+  pageProps: { ...pageProps },
+}) {
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  )
 }
 
-export default MyApp
