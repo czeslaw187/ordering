d641a861af92728d7d48302f867239a1064b781c
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-04-17 15:50:32 +0100

    created fooditem component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,24 @@
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {connect} from 'react-redux'
 
-function Navbar() {
+function Navbar(props) {
+    console.log(props.myState)
     return ( 
         <div className="w-full h-full py-5 flex flex-row justify-between bg-gradient-to-bl from-lime-100 to-green-300">
             <a className="mx-4" href="#">Order</a>
             <div>
                 <button className="mx-4"><FontAwesomeIcon icon={faCartShopping}/></button>
-                <div className="w-3/12 rounded-full bg-orange-400 relative z-[9999] -top-4 left-7 text-center text-xs m-0">1</div>
+                <div className="w-3/12 rounded-full bg-orange-400 relative z-[9999] -top-4 left-7 text-center text-xs m-0">{props.myState?.myState.length}</div>
             </div>
         </div>
      );
 }
 
-export default Navbar;
+function mapStateToProps(state) {
+    return {
+        myState: state
+    }
+}
+
+export default connect(mapStateToProps)(Navbar);
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,15 @@
 import menu from '../lib/menu.json'
-import Image from 'next/image';
-import {useRef} from 'react'
 import {connect} from 'react-redux'
+import FoodItem from '../components/FoodItem.js';
 
 function Home(props) {
   let state = props.state
-  console.log(props)
+  console.log(state)
   return ( 
     <div className="w-screen h-full overflow-y-auto text-xl bg-gradient-to-br from-slate-200 to-lime-300 rounded-md">
       <p className='w-full text-center text-3xl'>Starters</p>
       {menu.starters.map((el,id)=>{
-        const ref = useRef(null)
-        return (
-          <div  key={id}         
-                onClick={()=>{
-                  let foodItem = menu.starters.filter(el=>{return el.id == ref.current.childNodes[0].innerHTML[0]})
-                  props.addFood(foodItem)
-                }}
-                className='w-7/12 h-auto p-2 mx-auto hover:scale-[1.2] transition-transform 
-                                    delay-50 ease-out hover:border-gray-900 my-5 border-gray-300 
-                                    border-2 rounded-sm flex flex-row'>
-            <div className='my-1 mr-1'><Image src={el.image} width={150} height={120}/></div>
-            <div ref={ref}>
-              <p className='invisible'>{el.id}</p>
-              <p className='text-md m-2'>{el.name}</p>
-              <p className='text-sm'>{el.description}</p>
-              <p className='text-lg mt-5 ml-1'>£{el.price}.00</p>
-            </div>
-          </div>
-        )
+        return <FoodItem el={el} key={id} menu={menu} props={props} />
       })}
     </div>
    );
