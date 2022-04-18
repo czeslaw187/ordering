6eb8dbde41d2f7614a497e8e3fa81afd57f898db
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-04-18 10:23:35 +0100

    added total pay and pay button

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,10 +2,19 @@ import {connect} from 'react-redux'
 import Cart from './Cart.js';
 
 function Navbar(props) {
+    let total = props.myState?.myState.length > 0 ? props.myState?.myState.reduce((partialSum, a) => partialSum + a.data[0].price, 0) : [0]
     return ( 
         <div className="w-full h-[34] fixed mb-5 py-5 flex flex-row justify-between bg-gradient-to-bl from-lime-100 to-green-300">
             <a className="mx-4" href="#">Order</a>
-            <Cart props={props} />
+            <div className='flex flex-row-reverse'>
+                <Cart props={props} />                
+                <p className='mr-5 text-2xl'>
+                    {
+                       total > 0 ? `£${total}` : null
+                    }
+                </p>
+                {total > 0 ? <button className='w-[52] h-auto bg-indigo-600 rounded-lg px-10 mx-3'>Pay</button> : null}
+            </div>
         </div>
      );
 }
--- pages/api/assignId.js
+++ pages/api/assignId.js
@@ -2,7 +2,6 @@ export default async function(req, res) {
     const {item} = req.body
     try {        
         item[0].id = Date.now()
-        console.log(item)
         res.json(item)
     } catch(e) {
         res.json({message: e.message})
