05b8f11735b2525ed1a9d02fa05e51cd94777bb6
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-04-22 10:16:34 +0100

    investigating best place for fetchin data object for nodemailer

--- pages/api/myhook.js
+++ pages/api/myhook.js
@@ -29,6 +29,7 @@ const handler = async (req, res) => {
 
         if ( 'checkout.session.completed' === stripeEvent.type ) {
             const session = stripeEvent.data.object;
+            alert(session)    
         }
 
         res.json({ received: true });
