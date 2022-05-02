49c84670b4eda00e3d755bebbdf2948b0832df6e
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-05-02 01:34:15 +0100

    styling timeDist

--- components/Location.js
+++ components/Location.js
@@ -10,15 +10,15 @@ function Location({props}) {
 
     return ( 
         <div className={
-            visual ? "w-10/12 lg:w-5/12 h-2/6 opacity-100 rounded-xl absolute z-[10002] ml-8 lg:ml-14 transition-opacity duration-1000 bg-gray-900 border-slate-50 border-2 text-white":
+            visual ? "w-10/12 lg:w-5/12 h-2/6 opacity-100 rounded-xl absolute z-[10002] ml-8 lg:ml-14 transition-opacity duration-1000 bg-gradient-to-tr from-cyan-400 to-slate-400":
             "w-0 h-0 opacity-0"
         }>            
-            <div className='pb-4 mx-4 mb-4 border-2 border-slate-50 rounded-lg'>
+            <div className='rounded-lg border-2 border-indigo-600 h-5/6 m-3'>
                 <div className='w-full flex flex-row flex-end'>
                     <button className='ml-auto text-right mr-4' onClick={()=>{setVisual(false)}}>x</button>
                 </div>
-                <p className='w-full mt-4 text-center'>We are {tD ? Math.round(tD['length']/100)/10 : null} Km away from you</p>
-                <p className='w-full mt-4 text-center'>We can deliver your food in {tD ? Math.round(tD['duration']/60) : null} min</p>
+                <p className='w-full mt-4 text-center text-2xl'>We are {tD ? Math.round(tD['length']/100)/10 : null} Km away from you</p>
+                <p className='w-full mt-4 text-center text-2xl'>We can deliver your food in {tD ? Math.round(tD['duration']/60) : null} min</p>
             </div>
         </div>
      );
