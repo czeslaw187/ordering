4c8ec7ca9c2487ba1bc372e150a920bb1c15211a
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-05-03 00:57:01 +0100

    implementing data validation

--- pages/admin.js
+++ pages/admin.js
@@ -1,12 +1,37 @@
-import AdminLogin from "../components/adminComponents/AdminLogin.js";
+import ControlPanel from "../components/adminComponents/ControlPanel.js";
 import {connect} from 'react-redux'
+import {useState} from 'react'
 
 function Admin(props) {
     console.log(props.state, 'admin')
+    const [creden,setCreden] = useState({})
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(creden, 'admin')
+
+    }
+
+    const handleChange = (e) => {
+        const name = e.target.name
+        const value = e.target.value
+        setCreden(values=>({...values,[name]:value}))
+    }
+
     return ( 
-        <>
-            <AdminLogin props={props} />
-        </>
+        <div className="bg-gradient-to-tr from-sky-400 to-lime-500 w-screen h-screen flex-row">
+            {
+                !props.state.isLogged ?
+                <form className="flex flex-col w-3/12 h-2/6 justify-center mx-auto">
+                    <label className="font-serif text-lg mt-5" htmlFor="login">Login</label>
+                    <input className="pl-2 h-10 rounded-md" type="text" name="login" id="name" onChange={(e)=>handleChange(e)} required={true} />
+                    <label className="font-serif text-lg mt-5" htmlFor="password">Password</label>
+                    <input className="pl-2 h-10 rounded-md" type="password" name="password" id="password" onChange={(e)=>handleChange(e)} required={true} />
+                    <input type="submit" onClick={(e)=>{handleSubmit(e); props.isLogged(!props.state.isLogged)}} value={'Submit'} className="w-4/12 h-10 rounded-md text-center text-lg bg-lime-300 mx-auto mt-7" />
+                </form> :
+                <ControlPanel props={props} />
+            }
+        </div>
      );
 }
 
