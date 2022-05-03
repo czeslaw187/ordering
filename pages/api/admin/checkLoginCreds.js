c3a1d15fc78666d34402b7fdee26073e74575625
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-05-03 11:13:54 +0100

    conncted cred check to db and added error handling

--- pages/api/admin/checkLoginCreds.js
+++ pages/api/admin/checkLoginCreds.js
@@ -1,5 +1,33 @@
+import {sql_query} from '../../../lib/db.js'
+
 export default async function checkCredentials(req, res) {
     const {credentials} = req.body
-    console.log(credentials, 'api creds')
-    res.json(true)
+    const myName = process.env.NEXT_PUBLIC_MYNAME
+    
+    try {
+        let realCreds = await sql_query('SELECT login,password FROM myadmin WHERE name=?',[myName])
+        if (credentials.login === '') {
+            res.json({
+                message: 'Enter valid login',
+                logged: false
+            })
+        } else if (credentials.password === '') {
+            res.json({
+                message: 'Enter valid password',
+                logged: false
+            })
+        } else if (realCreds[0].login === credentials.login && realCreds[0].password === credentials.password) {
+            res.json({
+                message: 'ok',
+                logged: true
+            })
+        } else {
+            res.json({
+                message: 'wrong login/password',
+                logged: false
+            })
+        }
+    } catch(e) {
+        res.json({message: e.message})
+    }
 }   
