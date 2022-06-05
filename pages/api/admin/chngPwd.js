4924dc426fbea11acbd95fecc1343a68d46260fb
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-06-05 23:28:56 +0100

    add owner name for password change/check

--- pages/api/admin/chngPwd.js
+++ pages/api/admin/chngPwd.js
@@ -4,7 +4,7 @@ export default async function changePwd(req, res) {
     const{username, pass, conf} = req.body
     try {
         if (username) {
-            await sql_query('UPDATE myadmin SET login=? WHERE name="Grzegorz Michniewicz"', [username])
+            await sql_query('UPDATE myadmin SET login=? WHERE name=?', [username, process.env.NEXT_PUBLIC_MYNAME])
             res.json({message: 'username changed'})
         }
 
@@ -13,7 +13,7 @@ export default async function changePwd(req, res) {
         } else if (!pass || !conf) {
             res.json({message: 'enter credentials'})
         } else {
-            await sql_query('UPDATE myadmin SET password=? WHERE name="Grzegorz Michniewicz"',[pass])
+            await sql_query('UPDATE myadmin SET password=? WHERE name=?',[pass, process.env.NEXT_PUBLIC_MYNAME])
             res.json({message: 'password changed'})
         }
     } catch (error) {
--- pages/api/admin/getUsername.js
+++ pages/api/admin/getUsername.js
@@ -2,7 +2,7 @@ import { sql_query } from "../../../lib/db";
 
 export default async function getUsername(req, res) {
     try {
-        let response = await sql_query('SELECT * FROM myadmin WHERE name="Grzegorz Michniewicz"')
+        let response = await sql_query('SELECT * FROM myadmin WHERE name=?',[process.env.NEXT_PUBLIC_MYNAME])
         res.json(response)
     } catch (error) {
         res.json({message: error.message})
